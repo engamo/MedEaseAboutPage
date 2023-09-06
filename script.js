5e0564c78225989165e8ccc8581f07937774a2de
Mustapha Anthonio <engamo123@gmail.com>
2023-09-06 21:09:16 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// function slideRight(){
+//   const cards = document.getElementById('team');
+//   cards.scrollLeft += 300;
+// }
+
+// function slideLeft(){
+//   const cards = document.getElementById('article');
+//   cards.scrollLeft -= 300;
+// }
+
+function right() {
+  const cards = document.getElementById('team');
+  cards.scrollLeft += 300;
+}
+
+function left() {
+  const cards = document.getElementById('team');
+  cards.scrollLeft -= 300;
+}
