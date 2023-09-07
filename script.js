4a0d7685196153fa46f74ce0002be30df9f532f9
Mustapha Anthonio <engamo123@gmail.com>
2023-09-07 09:17:39 +0100

    updated

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-// function slideRight(){
-//   const cards = document.getElementById('team');
-//   cards.scrollLeft += 300;
-// }
-
-// function slideLeft(){
-//   const cards = document.getElementById('article');
-//   cards.scrollLeft -= 300;
-// }
-
 function right() {
   const cards = document.getElementById('team');
   cards.scrollLeft += 300;
